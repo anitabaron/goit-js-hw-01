9dc60d094f0be40a9a6edb88f6c16cdef35997f3
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-04-20 11:57:38 +0200

    update task-1 task-2 task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,14 @@
 function getElementWidth(content, padding, border) {
-  number.parseFloat;
+  return (
+    Number.parseFloat(content) +
+    Number.parseFloat(padding) * 2 +
+    Number.parseFloat(border) * 2
+  );
 }
-
-//50 + (2*8) + (2*4)
-//60 + (2*12) + (2*8,5)
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+//50 + (2*8) + (2*4)
+//60 + (2*12) + (2*8,5)
